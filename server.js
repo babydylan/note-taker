8d4e50cd8b0e416852234a60b3515816d74144bd
Dylan Nguyen <babydylannguyen@gmail.com>
2020-10-09 20:51:15 -0700

    add new file for routes

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const fs = require("fs")
 const express = require("express");
 const path = require("path");
-const app = express()
+const routes = require("./routes/routes");
+
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,56 +16,7 @@ app.use(
     })
 );
 
-//routes
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-function writeDataToDB(req, res, next) {
-    req.body.id = genID();
-    let newData = [...req.noteData, req.body];
-    fs.writeFile("./db/db.json", JSON.stringify(newData), (err, data) => {
-      if (!err) {
-        next();
-      } else console.log(err);
-    });
-}
-
-function getData(req, res, next) {
-    fs.readFile("./db/db.json", (err, data) => {
-      req.noteData = JSON.parse(data);
-      next();
-      if (err) {
-        res.sendStatus(404);
-      }
-    });
-}
-
-function deleteData(req, res, next) {
-    req.noteData = req.noteData.filter((note) => note.id !== req.params.id);
-    next();
-};
-
-module.exports = (app) => {
-    app.get("/api/notes", getData, (req, res) => {
-      res.send(req.noteData);
-    });
-  
-    app.post("/api/notes", getData, writeDataToDB, (req, res) => {
-      res.send(req.body);
-    });
-  
-    app.delete("/api/notes/:id", getData, deleteData, (req, res) => {
-      fs.writeFile("./db/db.json", JSON.stringify(req.noteData), (err) => {
-        if (!err) res.send(req.body);
-        else res.sendStatus(500)
-      });
-    });
-};
+routes(app);
 
 app.listen(PORT, () =>
   console.log(`Server listening at http://localhost:${PORT}`)
